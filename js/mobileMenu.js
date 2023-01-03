dfd396ef5fa10969cdd1451b5bcfb27fa1202eba
Zeph180 <kigozijosephsuubi1@gmail.com>
2023-01-03 17:14:25 +0300

    Add interactions

--- js/mobileMenu.js
+++ js/mobileMenu.js
@@ -1,8 +1,24 @@
-const  menu = document.getElementById('hamburger')
+const hamburger = document.getElementById('hamburger');
+const menu = document.getElementById('menu');
+const closeBtn = document.getElementById('close-btn');
+const menuOption1 = document.getElementById('menu-option1');
+const menuOption2 = document.getElementById('menu-option2');
+const menuOption3 = document.getElementById('menu-option3');
+const menuOption4 = document.getElementById('menu-option4');
 
-
+const menuOptions = [menuOption1, menuOption2, menuOption3, menuOption4];
 
 function openMenu() {
-  console.log("hello");
+  menu.style.display = 'block';
 }
-menu.addEventListener('click', openMenu)
+
+function closeMenu() {
+  menu.style.display = 'none';
+}
+
+for (let i = 0; i < menuOptions.length; i += 1) {
+  menuOptions[i].addEventListener('click', closeMenu);
+}
+
+hamburger.addEventListener('click', openMenu);
+closeBtn.addEventListener('click', closeMenu);
