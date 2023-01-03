54af851de35305f6e385ff2a55d95f3c632087e4
alicemirigo92 <alicemirigo92@gmai.com>
2023-01-03 07:59:13 +0300

    Add mobile items

--- js/mobileMenu.js
+++ js/mobileMenu.js
@@ -1,3 +1,24 @@
-const menu = document.getElementById('menu');
+// const menu = document.getElementById('menu');
 
-menu.style.backgroundColor = 'red';
+// menu.style.backgroundColor = 'red';
+const menu = document.getElementById('mobilemenu');
+const hamburger = document.getElementById('hamburger');
+
+menu.style.cssText = `
+  font-style: poppins, sans-serif;
+  margin-top: 30px;
+  list-style: none;
+  padding-left: 28px;
+  display: none;
+  position: fixed;
+`;
+
+function toggleMenu() {
+  if (menu.style.display === 'none') {
+    menu.style.display = 'block';
+  } else {
+    menu.style.display = 'none';
+  }
+}
+
+hamburger.addEventListener('click', toggleMenu);
