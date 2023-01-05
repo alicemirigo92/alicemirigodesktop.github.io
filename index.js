9884148f057f46c24d5b9f550a5aaf57376a0fdc
alicemirigo92 <alicemirigo92@gmai.com>
2023-01-05 22:12:04 +0300

    Add popup

---
+++ index.js
@@ -0,0 +1,87 @@
+const projects = {
+  project1: {
+    title: 'Project Name goes here',
+    img: './images/card-one.svg',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi',
+    stack: ['HTML/CSS', 'Ruby on rails', 'JavaScript'],
+    live: '#',
+    source: '#',
+  },
+  project2: {
+    title: 'Project Name goes here',
+    img: './images/card-two.svg',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi',
+    stack: ['HTML/CSS', 'Ruby on rails', 'JavaScript'],
+    live: '#',
+    source: '#',
+  },
+  project3: {
+    title: 'Project Name goes here',
+    img: './images/card-three.svg',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi',
+    stack: ['HTML/CSS', 'Ruby on rails', 'JavaScript'],
+    live: '#',
+    source: '#',
+  },
+  project4: {
+    title: 'Project Name goes here',
+    img: './images/card-four.svg',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi',
+    stack: ['HTML/CSS', 'Ruby on rails', 'JavaScript'],
+    live: '#',
+    source: '#',
+  },
+  project5: {
+    title: 'Project Name goes here',
+    img: './images/card-five.svg',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi',
+    stack: ['HTML/CSS', 'Ruby on rails', 'JavaScript'],
+    live: '#',
+    source: '#',
+  },
+  project6: {
+    title: 'Project Name goes here',
+    img: './images/card-six.svg',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi',
+    stack: ['HTML/CSS', 'Ruby on rails', 'JavaScript'],
+    live: '#',
+    source: '#',
+  },
+};
+ 
+const btns = document.querySelectorAll('.btn');
+btns.forEach((b) => {
+  b.addEventListener('click', () => {
+    const parent = b.parentNode;
+    const div = document.createElement('div');
+    div.className = 'popup';
+    div.innerHTML = `<div class="modalbox">
+<img id="cross" src="./images/ic_cross.svg" alt="ic_cross">
+        <p>${projects[b.id].title}</p>
+          <button class="modal-links" >${projects[b.id].stack[0]}</button>
+          <button class="modal-links" >${projects[b.id].stack[1]}</button>
+          <button class="modal-links" >${projects[b.id].stack[2]}</button>
+       <div class="modalimage"></div>
+        <p class="modal-text">${projects[b.id].description}</p>
+<div class="modal-btn">
+          <button type="button" onclick="location.href='${projects[b.id].live}';" class="live-btn">
+            See Live
+             <img id="arrow" src="./images/Union.svg" alt="arrow">
+          </button>
+          <button type="button" onclick="location.href='${projects[b.id].source}';" class="source-btn">See Source
+          </button>
+</div>
+    </div>`;
+    parent.append(div);
+ 
+    document.getElementById('cross').addEventListener('click', () => {
+      parent.removeChild(div);
+    });
+  });
+});
--- js/mobileMenu.js
+++ js/mobileMenu.js
@@ -22,3 +22,48 @@ for (let i = 0; i < menuOptions.length; i += 1) {
 
 hamburger.addEventListener('click', openMenu);
 closeBtn.addEventListener('click', closeMenu);
+
+const project1 = {
+  name: 'Project',
+  image: 'img-one img',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+  technology: ['Ruby on Rails', 'HTML/CSS', 'Javascript'],
+  projectno: 1,
+};
+const project2 = {
+  name: 'Project',
+  image: 'img-two img',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+  technology: ['Ruby on Rails', 'HTML/CSS', 'Javascript'],
+  projectno: 2,
+};
+const project3 = {
+  name: 'Project',
+  image: 'img-five img',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+  technology: ['Ruby on Rails', 'HTML/CSS', 'Javascript'],
+  projectno: 3,
+};
+const project4 = {
+  name: 'Project',
+  image: 'img-four img',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+  technology: ['Ruby on Rails', 'HTML/CSS', 'Javascript'],
+  projectno: 4,
+};
+const project5 = {
+  name: 'Project',
+  image: 'img-five img',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+  technology: ['Ruby on Rails', 'HTML/CSS', 'Javascript'],
+  projectno: 5,
+};
+const project6 = {
+  name: 'Project',
+  image: 'img-six img',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+  technology: ['Ruby on Rails', 'HTML/CSS', 'Javascript'],
+  projectno: 6,
+};
+
+const projectArray = [project1, project2, project3, project4, project5, project6];
